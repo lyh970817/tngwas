0bb7c053b2a55841a407b1b1b1cb899ddbeb38bc
Yuhao Lin <lyh970817@yandex.com>
2023-02-08 23:41:37 +0800

    Update plot.R

--- utils/plot.R
+++ utils/plot.R
@@ -1,3 +1,6 @@
+#!/usr/bin/env Rscript
+args <- commandArgs(trailingOnly=TRUE)
+
 if (length(args) != 6) {
   stop(
     "Specify input file, output directory, \
@@ -6,36 +9,38 @@ if (length(args) != 6) {
   )
 }
 
-sum <- data.table::fread(arg[1])
+print(args)
+sum <- data.table::fread(args[1])
 
 bitmap(
-  file = paste0(args[2], "man.png"), type = "png16m",
+  file = paste0(args[2], "/man.png"), type = "png16m",
   width = 7, height = 7, res = 300
 )
 par(cex.axis = 1.1, cex.lab = 1.3)
+
+
 qqman::manhattan(sum,
   suggestiveline = -log10(1e-05),
-  chr = arg[3], bp = arg[4], snp = arg[5], p = arg[6],
+  chr = args[3], bp = args[4], snp = args[5], p = args[6],
   col = c("#9CBEBD", "#95A39D"),
   cex = 0.5,
-  suggestiveline = -log10(1e-05),
   genomewideline = -log10(5e-08)
 )
 
 dev.off()
 
+
 bitmap(
-  file = paste0(args[2], "qq.png"), type = "png16m",
+  file = paste0(args[2], "/qq.png"), type = "png16m",
   width = 7, height = 7, res = 300
 )
+
+
 par(mar = c(5, 5, 1, 1))
-qqman::qq(sum[[arg[6]]],
-  cex.axis = 1.1,
-  cey.axis = 1.1,
-  cex.lab = 1.3,
-  cey.lab = 1.3,
+qqman::qq(sum[[args[6]]],
   xlim = c(0, 10),
   ylim = c(0, 10)
 )
 
 dev.off()
+
